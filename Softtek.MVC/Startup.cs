63704b71e8f013658b6ecd167e4362504956ac90
Alejandro <Alejandro@LAPTOP-BJID8VO1>
2017-12-28 20:40:32 -0600

    Add project files.

---
+++ Softtek.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Softtek.MVC.Startup))]
+namespace Softtek.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
